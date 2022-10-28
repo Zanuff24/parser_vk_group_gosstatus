96ee97a1445557e45fdec7ce33c4d0e702c1dc26
Zanuff24 <Zanuff24@gmail.com>
2022-10-28 17:10:08 +0300

    complite

--- project.py
+++ project.py
@@ -1,16 +1,45 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 import time
 import csv
 import config
 
+def openFile():
+    mas_url =[]
+    with open('list.csv', "r", encoding='cp1251') as fill:
+        reader = csv.reader(fill, delimiter=';')
+        for row in reader:
+            mas_url.append(row)
+    return mas_url
 
-#opensiteVK()
-#def opensiteVK():
+def saveFile(mas_url):
+    with open('list.csv', "w", encoding='cp1251', newline='') as fill:
+        writer = csv.writer(fill, delimiter=';')
+        writer.writerows(mas_url)
+
+def parse():
+    mas_url = openFile()
+    for i in mas_url:
+        status = checkStatus(i[0])
+        i.append(status)
+        time.sleep(3)
+        print(i,' status = ', status)
+    saveFile(mas_url)
+
+
+def checkStatus(link):
+    try:
+        time.sleep(2)
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR,'span[class="GovernmentCommunityBadge GovernmentCommunityBadge--tooltip"]')
+        return True
+    except:
+        print('error')
+        return False
+
 try:
     link_login='https://vk.com/'
-    link_pass=''
+
     browser = webdriver.Chrome()
     browser.implicitly_wait(5)
     browser.get(link_login)
@@ -23,11 +52,14 @@ try:
     input_password = browser.find_element(By.CSS_SELECTOR,'input[placeholder="Введите пароль"]')
     input_password.send_keys(config.pasword)
 
-    time.sleep(5)
+    time.sleep(3)
     button_auto = browser.find_element(By.CSS_SELECTOR,'button[type="submit"]')
     button_auto.click()
+
+    parse()
+
 finally:
     time.sleep(10)
-    #browser.quit()
+    browser.quit()
 
 
